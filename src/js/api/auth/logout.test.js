58999e638f374d637da921a0385a7d991f08dde5
aa096 <126397865+aa096@users.noreply.github.com>
2024-04-13 12:01:58 +0200

    added logout test file

---
+++ src/js/api/auth/logout.test.js
@@ -0,0 +1,20 @@
+import * as storage from "../../storage/index.js";
+import { logout } from "./logout.js";
+
+jest.mock("../../storage/index.js", () => ({
+  remove: jest.fn(),
+}));
+
+describe("Logout", () => {
+  beforeEach(() => {
+    storage.remove.mockClear();
+  });
+
+  it("should remove token and profile from localStorage", () => {
+    logout();
+
+    expect(storage.remove).toHaveBeenCalledTimes(2);
+    expect(storage.remove).toHaveBeenCalledWith("token");
+    expect(storage.remove).toHaveBeenCalledWith("profile");
+  });
+});
